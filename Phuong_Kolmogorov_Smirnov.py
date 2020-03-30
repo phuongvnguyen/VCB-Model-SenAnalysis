ec48d4e447a9130a7279e6692ac513d424002473
phuongvnguyen <31282453+phuongvnguyen@users.noreply.github.com>
2020-03-30 15:10:18 +0200

    Add files via upload

---
+++ Phuong_Kolmogorov_Smirnov.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import pandas as pd
+def ks(data=None,target=None, prob=None):
+    #import numpy as np
+    #import pandas as pd
+    data['target0'] = 1 - data[target]
+    data['bucket'] = pd.qcut(data[prob], 10)
+    grouped = data.groupby('bucket', as_index = False)
+    kstable = pd.DataFrame()
+    kstable['min_prob'] = grouped.min()[prob]
+    kstable['max_prob'] = grouped.max()[prob]
+    kstable['events']   = grouped.sum()[target]
+    kstable['nonevents'] = grouped.sum()['target0']
+    kstable = kstable.sort_values(by="min_prob", ascending=False).reset_index(drop = True)
+    kstable['event_rate'] = (kstable.events / data[target].sum()).apply('{0:.2%}'.format)
+    kstable['nonevent_rate'] = (kstable.nonevents / data['target0'].sum()).apply('{0:.2%}'.format)
+    kstable['cum_eventrate']=(kstable.events / data[target].sum()).cumsum()
+    kstable['cum_noneventrate']=(kstable.nonevents / data['target0'].sum()).cumsum()
+    kstable['KS'] = np.round(kstable['cum_eventrate']-kstable['cum_noneventrate'], 3) * 100
+
+    #Formating
+    kstable['cum_eventrate']= kstable['cum_eventrate'].apply('{0:.2%}'.format)
+    kstable['cum_noneventrate']= kstable['cum_noneventrate'].apply('{0:.2%}'.format)
+    kstable.index = range(1,11)
+    kstable.index.rename('Decile', inplace=True)
+    pd.set_option('display.max_columns', 9)
+    #print(kstable)
+    
+    #Display KS
+    from colorama import Fore
+    print(Fore.RED + "KS is " + str(max(kstable['KS']))+"%"+ " at decile " + str((kstable.index[kstable['KS']==max(kstable['KS'])][0])))
+    return(kstable)
+
+
+# In[ ]:
+
+
+
+
